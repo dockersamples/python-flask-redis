beb7e7cd7656a05895eb28ee1a7b8f27f2db095d
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2024-04-18 13:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from redis import Redis
+import os
+import socket
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host=os.environ.get('REDIS_HOST', '127.0.0.1'), port=6379)
+
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    counter = str(redis.get('hits'),'utf-8')
-    return "This webpage has been viewed "+counter+" time(s)"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    return f"This webpage has viewed {redis.get('hits').decode('utf-8')} times and hostname is {socket.gethostname()}.\n"
